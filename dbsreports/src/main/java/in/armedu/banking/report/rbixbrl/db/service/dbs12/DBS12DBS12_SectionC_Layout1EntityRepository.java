400408d757d7b5cee4e1e86558c88a80353ee800
digital@armedu.in <raam.mishra@swirecnco.com>
2022-12-16 13:40:52 +0530

    removed dbs12

--- dbsreports/src/main/java/in/armedu/banking/report/rbixbrl/db/service/dbs12/DBS12DBS12_SectionC_Layout1EntityRepository.java
+++
@@ -1,9 +0,0 @@
-package in.armedu.banking.report.rbixbrl.db.service.dbs12;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DBS12DBS12_SectionC_Layout1EntityRepository extends JpaRepository<DBS12DBS12_SectionC_Layout1Entity, Long> {
-}
--- dbsreports/src/main/java/in/armedu/banking/report/rbixbrl/db/service/dbs12/DBS12FilingInfo_Layout1EntityRepository.java
+++
@@ -1,9 +0,0 @@
-package in.armedu.banking.report.rbixbrl.db.service.dbs12;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DBS12FilingInfo_Layout1EntityRepository extends JpaRepository<DBS12FilingInfo_Layout1Entity, Long> {
-}
